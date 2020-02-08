9b67d9896efbf5a29e906bce49c2dcc9f50e1a50
Alec Smecher <alec@smecher.bc.ca>
2020-02-07 16:52:15 -0800

    pkp/pkp-lib#5488 Update copyright dates and GPLv2 license to GPLv3

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 /**
  * @file plugins/generic/usageStats/index.php
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_usageStats
  * @brief Wrapper for usage statistics plugin.
--- templates/privacyInformation.tpl
+++ templates/privacyInformation.tpl
@@ -1,9 +1,9 @@
 {**
  * plugins/generic/usageStats/templates/privacyInformation.tpl
  *
- * Copyright (c) 2013-2019 Simon Fraser University
- * Copyright (c) 2003-2019 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2013-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * Display usage stats privacy information and an opt-out option.
  *
